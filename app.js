0b2ee05483e685b3c4a4a302be6e004b681b1cd3
Faridur Rahman <ibnhosain014@gmail.com>
2026-04-21 00:02:22 +0800

    Add files via upload
    Simple bulb ON/OFF project using HTML, CSS, and JavaScript.

---
+++ app.js
@@ -0,0 +1,15 @@
+/*
+20/04/2026
+*/
+
+//Function
+function showDetails(){
+    //Variables
+   let myName = "ফরীদুর রহমান";
+   let myRole = "একজন ভবিষ্যৎ ওয়েব ডেভেলপার";
+
+
+   //DOM Manipulation
+   document.getElementById("welcome-smg").innerText ="স্বাগতম, আমি "+ myName;
+   document.getElementById("bio-ext").innerText ="আমি বর্তমানে "+ myRole +"।";
+}
